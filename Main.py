7410f91496f5f2c2fe2a3152051c731a5887f181
Lee junggeun <cjred77@gmail.com>
2016-12-27 22:31:22 +0900

    config 파일을 사용하여서 실행옵션지정 기능 추가 #5

--- Main.py
+++ Main.py
@@ -1,6 +1,9 @@
 import logging
 import Scraping
-import os, os.path
+import os
+import os.path
+import configparser
+
 from konlpy.tag import Hannanum
 from konlpy.utils import pprint
 from konlpy.tag import Kkma
@@ -13,13 +16,14 @@ COMPUTER_SECTION = "COMPUTER"
 FASION_SECTION = "FASION"
 DEFAULT_SCRAPING_SECTION = COMPUTER_SECTION
 
+
 def set_time_based_stamp():
     global stamp
     stamp = time.strftime("%Y%m%d%I%M", time.localtime())
     print(stamp)
 
 
-def setLogger():
+def set_logger():
     if not os.path.exists("logs/"):
         os.makedirs("logs/")
     # logging.basicConfig(filename="log.log", level=logging.DEBUG)
@@ -51,36 +55,60 @@ def get_nlpy():
     startTime = time.time()
     return k
 
-def get_section_idx():
+
+def get_section_idx(section):
     """
     나중에 DB와 연동하여 세션아이디를 가져오는것으로 수정예정
     """
     section_idx = 0
     section_map = {"COMPUTER":1, "FASION":2}
-    section_idx = section_map.get(COMPUTER_SECTION)
+    section_idx = section_map.get(section)
     return section_idx
 
 
 def get_feed_list():
-    feed_list = None
     with open("feed/feedlist.txt", 'r') as f:
         feed_list = f.readlines()
     return feed_list
 
 # http://api.egloos.com/lennis/post/6072774.xml
-def main():
-    set_time_based_stamp()
-    setLogger()
+
+
+def proc_kor(section_idx):
     k = get_nlpy()
     Scraping.set_knlpy(k)
-    #Scraping.set_stamp(stamp)
-    section_idx = get_section_idx()
+    # Scraping.set_stamp(stamp)
     Scraping.set_section_idx(section_idx)
 
     feed_list = get_feed_list()
     Scraping.proc_feed_list(feed_list)
-    #Scraping.get_rss_post_content(k, "http://blog.cjred.net/rss/")
-    #Scraping.get_egloos_post_content("lennis", "6072774")
+    # Scraping.get_rss_post_content(k, "http://blog.cjred.net/rss/")
+    # Scraping.get_egloos_post_content("lennis", "6072774")
+
+
+def proc_eng(section_idx):
+    print(section_idx)
+
+
+def main():
+    set_time_based_stamp()
+    set_logger()
+
+    config = configparser.ConfigParser()
+    config.read('config.conf')
+
+    mode = config.get('Default', 'mode')
+    section = config.get('Default', 'section')
+    section_idx = get_section_idx(section)
+
+    print('mode : ', mode)
+    if mode == 'KOR':
+        proc_kor(section_idx)
+    elif mode == 'ENG':
+        proc_eng(section_idx)
+    else:
+        print('unknown language.')
+
 
 if __name__ == "__main__":
     main()

a7b25322285f463c9be50c12a2855e94217f4736
Lee junggeun <cjred77@gmail.com>
2017-01-13 15:06:55 +0900

    keywords, eng_keyword 테이블 인덱스(keyword) 추가

--- db/scraping.sql
+++ db/scraping.sql
@@ -7,12 +7,14 @@ USE scraping;
 -- 테이블생성
 CREATE TABLE keywords (
   id INTEGER UNSIGNED AUTO_INCREMENT PRIMARY KEY ,
-  keyword VARCHAR(64) NOT NULL
+  keyword VARCHAR(64) NOT NULL,
+  UNIQUE INDEX keyword_idx(keyword)
 );
 
 CREATE TABLE eng_keywords (
   id INTEGER UNSIGNED AUTO_INCREMENT PRIMARY KEY ,
-  keyword VARCHAR(64) NOT NULL
+  keyword VARCHAR(64) NOT NULL,
+  UNIQUE INDEX keyword_idx(keyword)
 );
 
 CREATE TABLE sections (
@@ -62,14 +64,17 @@ WHERE table_schema = "scraping"
 INSERT INTO sections(section)  values('COMPUTER');
 INSERT INTO sections(section)  values('FASHION');
 
+
+-- Test
 select * from eng_keywords
 where 1>0
- and id in (176, 877)
+  and id in (496)
+-- and keyword like '\%'
 ;
 select * from keywords
 where 1>0
--- and id in (708,3)
-and keyword='사용'
+ and id in (126,964)
+-- and keyword='사용'
 ORDER BY id ASC;
 
 SELECT * FROM sections where section ='FASHION';
@@ -81,15 +86,29 @@ SELECT * FROM cnts ORDER BY cnt DESC ;
 
 -- table 정보 삭제
 DELETE From eng_cnts
-where keyword_id=877;
-DELETE from eng_keywords;
+where keyword_id=230;
+DELETE from eng_keywords
+WHERE id=230;
 COMMIT;
 
-
-
-SELECT keyword_id, cnt FROM eng_cnts
+SELECT a.keyword, a.id, b.cnt
+FROM
+  (SELECT id, keyword FROM keywords) a,
+  (SELECT keyword_id, cnt FROM cnts
 WHERE section_id = 1
 -- GROUP BY section_id
 ORDER BY cnt DESC
- LIMIT 10
+ LIMIT 100) b
+WHERE a.id = b.keyword_id
+;
+
+SELECT a.keyword, a.id, b.cnt
+FROM
+  (SELECT id, keyword FROM eng_keywords) a,
+  (SELECT keyword_id, cnt FROM eng_cnts
+WHERE section_id = 1
+-- GROUP BY section_id
+ORDER BY cnt DESC
+ LIMIT 100) b
+WHERE a.id = b.keyword_id
 ;
